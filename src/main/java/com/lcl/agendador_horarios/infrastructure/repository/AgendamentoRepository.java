a0fa51c30ff360aa9b6210324b0074750f4acf9c
Alfredo <alfrdons.jr@gmail.com>
2026-06-15 22:48:53 -0300

    update AgendamentoRepository to return a list of Agendamentos for date range queries

--- src/main/java/com/lcl/agendador_horarios/infrastructure/repository/AgendamentoRepository.java
+++ src/main/java/com/lcl/agendador_horarios/infrastructure/repository/AgendamentoRepository.java
@@ -5,6 +5,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
     Agendamento findByServicoAndDataHoraAgendamentoBetween(String servico, LocalDateTime dataHoraInicio, LocalDateTime dataHoraFinal);
@@ -12,7 +13,7 @@ public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> 
     @Transactional
     void deleteByDataHoraAgendamentoAndCliente(LocalDateTime dataHoraAgendamento, String cliente);
 
-    Agendamento findByDataHoraAgendamentoBetween(LocalDateTime dataHoraInicial, LocalDateTime dataHoraFinal);
+    List<Agendamento> findByDataHoraAgendamentoBetween(LocalDateTime dataHoraInicial, LocalDateTime dataHoraFinal);
 
     Agendamento findByDataHoraAgendamentoAndCliente(LocalDateTime dataHoraAgendamento, String cliente);
 }
